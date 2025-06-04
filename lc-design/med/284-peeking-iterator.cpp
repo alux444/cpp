e9f402d3ea49cc3b1f21bc13f796cdb6ab7111c5
Alex Liang <126375355+alux444@users.noreply.github.com>
2025-06-04 18:52:47 +1200

    added sum qs

---
+++ lc-design/med/284-peeking-iterator.cpp
@@ -0,0 +1,70 @@
+#include <vector>
+
+/*
+ * Below is the interface for Iterator, which is already defined for you.
+ * **DO NOT** modify the interface for Iterator.
+ *
+ *  class Iterator {
+ *		struct Data;
+ * 		Data* data;
+ *  public:
+ *		Iterator(const vector<int>& nums);
+ * 		Iterator(const Iterator& iter);
+ *
+ * 		// Returns the next element in the iteration.
+ *		int next();
+ *
+ *		// Returns true if the iteration has more elements.
+ *		bool hasNext() const;
+ *	};
+ */
+
+class PeekingIterator : public Iterator
+{
+  int peeked;
+  bool hasPeeked;
+
+public:
+  PeekingIterator(const std::vector<int> &nums) : Iterator(nums)
+  {
+    // Initialize any member here.
+    // **DO NOT** save a copy of nums and manipulate it directly.
+    // You should only use the Iterator interface methods.
+    if (Iterator::hasNext())
+    {
+      peeked = Iterator::next();
+      hasPeeked = true;
+    }
+  }
+
+  // Returns the next element in the iteration without advancing the iterator.
+  int peek()
+  {
+    if (hasPeeked)
+    {
+      return peeked;
+    }
+    // have not peeked
+    peeked = Iterator::next();
+    hasPeeked = true;
+    return peeked;
+  }
+
+  // hasNext() and next() should behave the same as in the Iterator interface.
+  // Override them if needed.
+  int next()
+  {
+    if (hasPeeked)
+    {
+      hasPeeked = false;
+      return peeked;
+    }
+    // hasn't peeked
+    return Iterator::next();
+  }
+
+  bool hasNext() const
+  {
+    return (!hasPeeked && Iterator::hasNext()) || (hasPeeked);
+  }
+};
